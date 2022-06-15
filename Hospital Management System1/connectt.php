f6d7bc762be27d443e8c59e36ea2dfa3508ee5d1
reemaalghzaly <ralghazali@students.iugaza.edu.ps>
2022-06-15 04:40:33 +0300

    first

--- Hospital Management System1/connectt.php
+++ Hospital Management System1/connectt.php
@@ -1,17 +1,17 @@
 <?php
-	$con = mysqli_connect("localhost","root","");
+	$con = mysqli_connect("us-cdbr-east-05.cleardb.net","bc49f1ed20ea78","73cc4abb");
 	date_default_timezone_set ("Asia/Colombo");
-	mysqli_select_db($con,"hospitol");
+	mysqli_select_db($con,"heroku_d87e205df202950");
 	if(!$con){
 			die("Failed to connect");
 			}
 
 ?>
 <?php
-define ("HOSTNAME","localhost");
-define ("USERNAME","root");
-define ("PASSWORD","");
-define ("DATABASE","hospitol");
+define ("HOSTNAME","us-cdbr-east-05.cleardb.net");
+define ("USERNAME","bc49f1ed20ea78");
+define ("PASSWORD","73cc4abb");
+define ("DATABASE","heroku_d87e205df202950");
 
 $dbhandle=new mysqli(HOSTNAME,USERNAME,PASSWORD,DATABASE) or die("unable to connect to database");
 
